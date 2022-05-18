1fe24802a3ae76f60ea11719198dd06603a7508e
dayanels <lemos.dayane.d@gmail.com>
2022-05-18 15:54:40 -0300

    Aula 5 - finalizado

--- main.js
+++ main.js
@@ -1,11 +1,41 @@
 window.addEventListener("scroll", onScroll);
 
-
 onScroll();
 
 function onScroll() {
   showNavOnScroll();
   showBackToTopButtonOnScroll();
+
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(contact);
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+  // Verificar se a sessão passou da linha
+
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+
+  //verificar se a base está abaixo da linha
+
+  const sectionEndsAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+
+  // limetes da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
+
+  const sectionId = section.getAttribute('id');
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if(sectionBoundaries){
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
